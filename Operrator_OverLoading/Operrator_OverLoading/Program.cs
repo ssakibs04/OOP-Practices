9d93ebfa5a07d0902fec29fb5ca77b60b2a42ac9
ssakibs04 <sakibshadman34@gmail.com>
2024-02-17 11:52:39 +0600

    Updated

---
+++ Operrator_OverLoading/Operrator_OverLoading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OperatorOverloading
+{
+    class oprtr
+    {
+        public string name;
+        public int num;
+
+        // Overloading the + operator
+        public static oprtr operator +(oprtr firstName, oprtr lastName)
+        {
+            oprtr fullName = new oprtr();
+            fullName.name = firstName.name + " " + lastName.name;
+            fullName.num = firstName.num + lastName.num;
+            return fullName;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            oprtr firstName = new oprtr();
+            firstName.name = "Sadman";
+            firstName.num = 10;
+
+            oprtr lastName = new oprtr();
+            lastName.name = "Sakib";
+            lastName.num = 20;
+
+            oprtr fullName = firstName + lastName;
+
+            Console.WriteLine(fullName.name);
+            Console.WriteLine(fullName.num);
+
+            Console.ReadLine();
+        }
+    }
+}
